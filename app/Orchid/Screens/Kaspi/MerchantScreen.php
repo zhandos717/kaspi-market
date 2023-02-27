767d9a48f1b6b3a4b63ba96150b12ecbbece86f9
zhandos <zh.zhandarbekov@wiponapp.com>
2023-02-28 01:47:45 +0600

    init

---
+++ app/Orchid/Screens/Kaspi/MerchantScreen.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Orchid\Screens\Kaspi;
+
+use App\Services\Kaspi\Merchant\Collector;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Orchid\Screen\Actions\ModalToggle;
+use Orchid\Screen\Fields\Input;
+use Orchid\Screen\Repository;
+use Orchid\Screen\Screen;
+use Orchid\Screen\TD;
+use Orchid\Support\Facades\Layout;
+
+class MerchantScreen extends Screen
+{
+    public function query(): iterable
+    {
+        $item = \Cache::get('item');
+
+        if(empty($item)){
+            return [];
+        }
+
+        $data = (new Collector($item['url'],$item['city_id']))->handle();
+
+        return [
+            'table'   => $data
+        ];
+    }
+
+    public function name(): ?string
+    {
+        return 'Kaspi продавцы';
+    }
+    public function description(): ?string
+    {
+        return 'Данные парсинга';
+    }
+    public function commandBar(): iterable
+    {
+        return [
+            ModalToggle::make('Добавить данные для парсинга')
+                ->modal('exampleModal')
+                ->method('showToast')
+                ->icon('plus'),
+        ];
+    }
+    public function layout(): iterable
+    {
+        return [
+            Layout::table('table', [
+                TD::make('masterCategory', 'Категория')
+                    ->render(fn($data) => $data['masterCategory']),
+
+                TD::make('merchantName', 'Продавец')
+                    ->render(fn($data) => $data['merchantName']),
+
+                TD::make('price', 'Цена')
+                   ->render(fn($data) => number_format($data['price'], 2)),
+            ]),
+
+            Layout::modal(
+                'exampleModal',
+                Layout::rows([
+                    Input::make('url')
+                        ->title('url')
+                        ->placeholder('http://example.com')
+                        ->required(),
+
+                    Input::make('city_id')
+                        ->title('city')
+                        ->placeholder('710000000')
+                        ->required(),
+                ])
+            )->title('Введите данные для получения цен'),
+        ];
+    }
+
+    public function showToast(Request $request)
+    {
+
+
+
+        \Cache::set('item',[
+            'url'=>$request->url,
+            'city_id'=>$request->city_id,
+        ]);
+    }
+}
---
+++ app/Services/Kaspi/Merchant/Collector.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services\Kaspi\Merchant;
+
+use App\Models\City;
+use App\Models\Merchant;
+use Illuminate\Support\Facades\Http;
+
+class Collector
+{
+
+    public function __construct(private readonly string $link, private readonly ?string $cityId)
+    {
+    }
+
+    public function handle()
+    {
+        preg_match_all('/\d{6,9}/', $this->link, $output_array);
+
+        [$id, $cityId] = array_shift($output_array);
+
+
+        $response = Http::contentType('application/json; charset=UTF-8')->
+        withHeaders([
+            'Referer' => $this->link
+        ])->
+        post('https://kaspi.kz/yml/offer-view/offers/' . $id, [
+            "cityId" => $this->cityId ?? $cityId,
+            "id" => (string)$id,
+            "merchantUID" => "",
+            "limit" => 64,
+            "page" => 0,
+            "sort" => true
+        ]);
+
+        return array_filter(
+            $response->json()['offers'],
+            function ($offer) {
+                return $offer['kaspiDelivery'] == false;
+            }
+        );
+    }
+}
