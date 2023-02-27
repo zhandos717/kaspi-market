c46e2dc77535d4c41eb286a62f065da1328e2c24
zhandos <zh.zhandarbekov@wiponapp.com>
2023-02-28 01:51:09 +0600

    init

---
+++ app/Orchid/PlatformProvider.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Orchid;
+
+use Orchid\Platform\Dashboard;
+use Orchid\Platform\ItemPermission;
+use Orchid\Platform\OrchidServiceProvider;
+use Orchid\Screen\Actions\Menu;
+use Orchid\Support\Color;
+
+class PlatformProvider extends OrchidServiceProvider
+{
+    /**
+     * @param Dashboard $dashboard
+     */
+    public function boot(Dashboard $dashboard): void
+    {
+        parent::boot($dashboard);
+        // ...
+    }
+
+    /**
+     * @return Menu[]
+     */
+    public function registerMainMenu(): array
+    {
+        return [
+            Menu::make('kaspi')
+                ->icon('monitor')
+                ->route('kaspi.merchant')
+                ->title('Market')
+        ];
+    }
+
+    /**
+     * @return Menu[]
+     */
+    public function registerProfileMenu(): array
+    {
+        return [
+            Menu::make(__('Profile'))
+                ->route('platform.profile')
+                ->icon('user'),
+        ];
+    }
+
+    /**
+     * @return ItemPermission[]
+     */
+    public function registerPermissions(): array
+    {
+        return [
+            ItemPermission::group(__('System'))
+                ->addPermission('platform.systems.roles', __('Roles'))
+                ->addPermission('platform.systems.users', __('Users')),
+        ];
+    }
+}
